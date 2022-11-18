de5f5673c5f8aed972314e023685ca4914ab62b8
joao-pedro-laureano <joaopedrolco@hotmail.com>
2022-11-18 03:38:55 -0300

    Add project files.

---
+++ bytebank/Program.cs
@@ -0,0 +1,19 @@
+﻿using bytebank;
+
+ContaCorrente contaDoAndre = new ContaCorrente();
+contaDoAndre.titular = "André Dias";
+contaDoAndre.numero_agencia = 22;
+contaDoAndre.conta = "1234-5";
+contaDoAndre.saldo = 250;
+
+ContaCorrente contaDaMaria = new ContaCorrente();
+contaDaMaria.titular = "Maria Noites";
+contaDaMaria.numero_agencia = 13;
+contaDaMaria.conta = "5454-6";
+contaDaMaria.saldo = 100;
+
+contaDoAndre.Depositar(150);
+contaDoAndre.Transferir(45, contaDaMaria);
+
+Console.WriteLine($"O saldo da conta do André é de {contaDoAndre.saldo}");
+Console.WriteLine($"O saldo da conta da Maria é de {contaDaMaria.saldo}");
